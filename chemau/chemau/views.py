11dbd347b8cf7594913604e85133b8f0117785b9
HARITIANA <haritiananomenjanahary3@gmail.com>
2023-12-01 07:29:12 +0300

    Mon premier commite Projet Dango

---
+++ chemau/chemau/views.py
@@ -0,0 +1,10 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home_view(request):
+    #return HttpResponse("Mon premer page web")
+    return render(request, 'home.html')
+
+def contact_view(request):
+    #return HttpResponse("Mon page de contact")
+    return render(request, 'contact.html')
